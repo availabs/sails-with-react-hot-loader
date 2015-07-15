60b5fb55d36b5877fcad5ec960fe10a6e8dd0e8f
Paul Tomchik <pauljtomchik@gmail.com>
2015-07-15 17:42:50 -0400

    Modified compileAssets.

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -1,10 +1,17 @@
+"use strict";
+
+var compileAssets = [
+    'clean:dev',
+    'jst:dev',
+    'less:dev',
+    'copy:dev',
+    'coffee:dev',
+];
+
+if (process.env.NODE_ENV === 'development') {
+    compileAssets[compileAssets.length] = 'exec:webpackDevServerLift';
+}
+
 module.exports = function (grunt) {
-    grunt.registerTask('compileAssets', [
-        'clean:dev',
-        'jst:dev',
-        'less:dev',
-        'copy:dev',
-        'coffee:dev',
-        'exec:webpackDevServerLift',
-    ]);
+    grunt.registerTask('compileAssets', compileAssets);
 };
